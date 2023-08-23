e65da464358de9f90d90f235d521f48dcefc1094
Jordan8899 <burmei.jorda@awatapu.school.nz>
2023-08-24 09:55:17 +1200

    Commented Code

--- Racing_Game/Racing_Game.py
+++ Racing_Game/Racing_Game.py
@@ -3,6 +3,51 @@ import pygame
 
 # Constants
 
+
+# Pygame Initilisation so that pygame works through the code
+
+# Colour Dictonary with colour values used within this game
+
+# Game Icon and Name
+
+
+# Fonts
+
+# Starting Score
+
+
+
+
+
 # Functions
 
+
+# Movement Function
+
+
+
+
 # Main Routine
+
+
+# Play Again Loop
+
+# Player Car Model
+
+# Other Racers Model
+
+# Other Racers Movement
+
+# Player Movement and Limiter Controller
+
+# Score
+
+# Highscore
+
+# Road Creation
+
+# Lanes Creation
+
+# Background Creation
+
+# Game Over
